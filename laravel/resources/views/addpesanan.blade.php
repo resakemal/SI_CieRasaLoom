ffa4031c921975633419e6fe199055957a37137f
alvinjunianto <alvinjunianto@gmail.com>
2017-04-12 16:50:34 +0700

    add many things

--- laravel/resources/views/addpesanan.blade.php
+++ laravel/resources/views/addpesanan.blade.php
@@ -1,37 +1,3 @@
-<!--<html>
-
-{!! Html::script('js/updatetable.js'); !!}
-    <body>
-        {!! Form::open(['url' => '/addpesanan/submit']) !!}
-
-        {!! Form::label('pemasok_id', 'Nama Pemasok') !!}
-        {!! Form::select('pemasok_id', $inputs) !!}
-        <br>
-        {!! Form::label('nama_barang_lbl', 'Nama Barang') !!}
-        {!! Form::text('nama_barang') !!}
-        <br>
-        {!! Form::label('jum_barang_lbl', 'Jumlah Barang') !!}
-        {!! Form::text('jum_barang') !!}
-        <br>
-
-        <input type="button" value="Add Barang" onclick="updateTable();">
-
-        <table id="tabel_barang">
-            <thead>
-                <tr>
-                    <th>Nama Barang</th>
-                    <th>Jumlah Barang</th>
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-
-        {!! Form::submit('Submit') !!}
-        {!! Form::close() !!}
-    </body>
-</html>
--->
 @extends('layouts.app')
 
 @section('content')
@@ -44,31 +10,50 @@
                 <div class="panel-body">
                    
                     {!! Form::open(['url' => '/addpesanan/submit']) !!}
-                    <div class="u1">
-                    {!! Form::label('pemasok_id', 'Nama Pemasok') !!}
+                    <div style="display:inline;">
+                        {!! Form::label('pemasok_id', 'Nama Pemasok: ') !!}
                     </div>
-                    {!! Form::select('pemasok_id', $inputs) !!}
-                    <br>
-                    {!! Form::label('nama_barang_lbl', 'Nama Barang') !!}
-                    {!! Form::text('nama_barang') !!}
-                    <br>
-                    {!! Form::label('jum_barang_lbl', 'Jumlah Barang') !!}
-                    {!! Form::text('jum_barang') !!}
-                    <br>
+                    
+                    <div style="display:inline;padding-left: 1.8em">
+                        {!! Form::select('pemasok_id', $inputs) !!}
+                    </div>
 
+                    <br><br>
+                    <div style="display:inline;padding-right: 0.6em">
+                        {!! Form::label('nama_barang_lbl', 'Nama Barang: ') !!}
+                    </div>
+                    <div style="display:inline;padding-left: 1.8em">
+                        {!! Form::text('nama_barang') !!}
+                    </div>
+
+                    <br><br>
+                    <div style="display:inline;">
+                        {!! Form::label('jum_barang_lbl', 'Jumlah Barang: ') !!}
+                    </div>
+                    <div style="display:inline;padding-left: 1.8em">
+                        {!! Form::text('jum_barang') !!}
+                    </div>
+                    
+                    <br><br>
                     <input type="button" value="Add Barang" onclick="updateTable();">
 
-                    <table id="tabel_barang">
-                        <thead>
-                            <tr>
-                                <th>Nama Barang</th>
-                                <th>Jumlah Barang</th>
+                    <br><br>
+                    <table id="tabel_barang" style="border: 1px solid #ccc; margin: 3px ; padding: 3px; width: 500px; text-align: center;">
+                        <thead style="text-align: center;">
+                            <tr style="border: 1px solid #ccc;">
+                                <th style="text-align: center; border: 1px solid #ccc;">Nama Barang</th>
+                                <th style="text-align: center; border: 1px solid #ccc;">Jumlah Barang</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody style="border: 1px solid #ccc; height:50%;">
+                            <tr style="border: 1px solid #ccc;">
+                                <td style="text-align: center; border: 1px solid #ccc;"></td>
+                                <td style="text-align: center; border: 1px solid #ccc;"></td>
+                            </tr>
                         </tbody>
                     </table>
 
+                    <br><br><br><br>
                     {!! Form::submit('Submit') !!}
                     {!! Form::close() !!}
 
--- laravel/resources/views/daftarpesanan.blade.php
+++ laravel/resources/views/daftarpesanan.blade.php
@@ -1,29 +1,41 @@
-<html>
-    <body>
-    	<ul>
-        @foreach($inputs as $input)
-        	<li>
-        		<div>
-                    {!! Form::label('id_pesanan', 'ID Pesanan: ') !!}
-        			{!! $input->pesanan_id !!}
-        			<br>
-                    {!! Form::label('waktu_pesanan', 'Waktu Pesanan: ') !!}
-        			{!! $input->created_at !!} 
-                    <br>
-                    {!! Form::label('status_pesanan', 'Status: ') !!}
-                    @if($input->status === 1)       Pesanan dikirim
-                    @elseif($input->status === 2)   Pesanan diterima
-                    @elseif($input->status === 3)   Barang dikirim
-                    @elseif($input->status === 4)   Barang diterima
-                    @endif
-                    <br>
-                    {!! Html::linkAction('PesananController@status','Edit',['id' => $input->pesanan_id]) !!}
-        		</div>
-        	</li>
-        @endforeach
-    	</ul>
-        {!! Form::open(['url' => '/addpesanan']) !!}
-        {!! Form::submit('Add') !!}
-        {!! Form::close() !!}
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Daftar Pesanan Bahan Baku</b></div>
+
+                <div class="panel-body">
+                	<ul>
+                    @foreach($inputs as $input)
+                    	<li>
+                    		<div>
+                                {!! Form::label('id_pesanan', 'ID Pesanan: ') !!}
+                    			{!! $input->pesanan_id !!}
+                    			<br>
+                                {!! Form::label('waktu_pesanan', 'Waktu Pesanan: ') !!}
+                    			{!! $input->created_at !!} 
+                                <br>
+                                {!! Form::label('status_pesanan', 'Status: ') !!}
+                                @if($input->status === 1)       Pesanan dikirim
+                                @elseif($input->status === 2)   Pesanan diterima
+                                @elseif($input->status === 3)   Barang dikirim
+                                @elseif($input->status === 4)   Barang diterima
+                                @endif
+                                <br>
+                                {!! Html::linkAction('PesananController@status','Edit',['id' => $input->pesanan_id]) !!}
+                    		</div>
+                    	</li>
+                    @endforeach
+                	</ul>
+                    {!! Form::open(['url' => '/addpesanan']) !!}
+                    {!! Form::submit('Add') !!}
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
