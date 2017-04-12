0f53cb84fd52adb560d5826210b9a186302fdfb9
alvinjunianto <alvinjunianto@gmail.com>
2017-04-12 20:01:23 +0700

    styling

--- laravel/resources/views/daftartransaksi.blade.php
+++ laravel/resources/views/daftartransaksi.blade.php
@@ -8,15 +8,17 @@
                 <div class="panel-heading"><b>Halaman Utama</b></div>
 
                 <div class="panel-body">
-                	<table name='daftar_transaksi'>
-                    <tr>
-                        <th>Waktu Transaksi</th>
-                        <th>Total Harga</th>
-                    </tr>
+                	<table name='daftar_transaksi' style="border: 1px solid #ccc; margin: 3px ; padding: 3px; width: 500px; text-align: center;">
+                    <thead style="text-align: center;">
+                        <tr style="border: 1px solid #ccc;">
+                            <th style="text-align: center; border: 1px solid #ccc;">Waktu Transaksi</th>
+                            <th style="text-align: center; border: 1px solid #ccc;">Total Harga</th>
+                        </tr>
+                    </thead>
                     @foreach($inputs as $input)
-                    <tr>
-                        <td>{!! $input->created_at !!}</td>
-                        <td>{!! $input->total_harga !!}</td>
+                    <tr style="border: 1px solid #ccc;">
+                        <td style="text-align: center; border: 1px solid #ccc;">{!! $input->created_at !!}</td>
+                        <td style="text-align: center; border: 1px solid #ccc;">{!! $input->total_harga !!}</td>
                     </tr>
                     @endforeach
                 </div>
