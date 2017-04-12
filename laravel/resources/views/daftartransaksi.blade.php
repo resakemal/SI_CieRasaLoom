f230c9012b57f1295078296eb2dba4b0194cb2e5
resakemal <resakemal@gmail.com>
2017-04-12 19:52:43 +0700

    Laporan Transaksi View Update

--- laravel/resources/views/daftartransaksi.blade.php
+++ laravel/resources/views/daftartransaksi.blade.php
@@ -1,15 +1,27 @@
-<html>
-    <body>
-    	<table name='daftar_transaksi'>
-        <tr>
-            <th>Waktu Transaksi</th>
-            <th>Total Harga</th>
-        </tr>
-        @foreach($inputs as $input)
-        <tr>
-            <td>{!! $input->created_at !!}</td>
-            <td>{!! $input->total_harga !!}</td>
-        </tr>
-        @endforeach
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"><b>Halaman Utama</b></div>
+
+                <div class="panel-body">
+                	<table name='daftar_transaksi'>
+                    <tr>
+                        <th>Waktu Transaksi</th>
+                        <th>Total Harga</th>
+                    </tr>
+                    @foreach($inputs as $input)
+                    <tr>
+                        <td>{!! $input->created_at !!}</td>
+                        <td>{!! $input->total_harga !!}</td>
+                    </tr>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
